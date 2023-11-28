cbd8e1d7929b2a9646350cc00de7216aa9b4f796
farruk12 <farruk.omar@icloud.com>
2023-11-28 10:31:43 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
+for i in range(ord('z'), ord('A'), -1):
     if ord('a') <= i <= ord('z') or ord('A') <= i <= ord('Z'):
         print("{:c}".format(i), end="")
