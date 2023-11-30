8f51ee23dbea1bac31e4d99bb43890c783ad0d3f
farruk12 <farruk.omar@icloud.com>
2023-11-30 08:10:13 -0800

    1-calculation.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,7 @@ from sys import argv
 
 if __name__ == "__main__":
     num_args = len(argv) - 1
-    print("{} argument{}.".format(num_args, '' if num_args == 1 else 's'))
+    print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''), end="")
+    print(" ." if num_args == 0 else "")
     for i, arg in enumerate(argv[1:], start=1):
         print("{}: {}".format(i, arg))
