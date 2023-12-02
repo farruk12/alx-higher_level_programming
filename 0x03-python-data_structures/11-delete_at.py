30e3190256c880421849532660c6e0b53f9fb3ad
farruk12 <farruk.omar@icloud.com>
2023-12-02 12:48:16 -0800

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        for i, element in enumerate(my_list):
+            if idx == i:
+                my_list.remove(element)
+        return my_list
