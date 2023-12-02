b204e38d0cc0118e5a72f883ef8eca25385ebe84
farruk12 <farruk.omar@icloud.com>
2023-12-02 09:46:55 -0800

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    x = len(sentence)
+
+    if x == 0:
+         result_tuple =  (0, None)
+    else:
+        first_char = sentence[0]
+        result_tuple = (x, first_char)
+
+    return result_tuple
