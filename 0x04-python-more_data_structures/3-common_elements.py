b40f0d390dda0209f1004413763f9dc62b9289f9
farruk12 <farruk.omar@icloud.com>
2023-12-06 03:17:34 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for x in set_2:
-            if i == x:
-                return i
+    x = set_1.intersection(set_2)
+    return x
