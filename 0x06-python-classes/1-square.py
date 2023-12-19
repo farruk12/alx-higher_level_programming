5dba63e878f72df9cf43cad547e1c3c876a6d03c
farruk12 <farruk.omar@icloud.com>
2023-12-19 11:24:57 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Square module that has size
+
+"""
+
+
+class Square:
+    """
+    A class square that has size as an instance attribute
+
+    """
+
+    def __init__(self, size):
+        """
+        Insanitation with size
+        """
+
+        self.__size = size
+        """
+        private instance attribute: size
+        """
