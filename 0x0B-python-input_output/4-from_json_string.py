753f78da2fe5f1c50bf4c89bb1701864e455613d
farruk12 <farruk.omar@icloud.com>
2024-01-09 04:55:04 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""Module: my_module"""
 
+"""Defines a JSON-to-object function."""
 import json
 
+
 def from_json_string(my_str):
-    """Converts a JSON string to a Python data structure.
-
-    Args:
-        my_str (str): A JSON-formatted string.
-
-    Returns:
-        object: A Python data structure representing the JSON string.
-
-    Raises:
-        ValueError: If the input string is not a valid JSON.
-    """
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
