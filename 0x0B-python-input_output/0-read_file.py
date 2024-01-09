3b43e8f4d1c9d8b7e898f4f3fe7e24edf43b6691
farruk12 <farruk.omar@icloud.com>
2024-01-09 04:23:26 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-"""Defines an object attribute lookup function."""
+"""Defines a text file-reading function"""
 
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+def read_file(filename=""):
+    """Print the contests of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
